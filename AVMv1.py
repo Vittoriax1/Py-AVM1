ddafd529a7334f5dd70f30ee10b2fa6d1251ff69
Vittoriax1 <99565350+Vittoriax1@users.noreply.github.com>
2022-07-01 16:02:10 -0400

    Add files via upload

---
+++ AVMv1.py
@@ -0,0 +1,8 @@
+# This is Version 1, created by Jennifer Souvannasing
+animal = 'Chicken'
+vegetable = 'Eggplant'
+mineral = 'Gold'
+print('Here is an animal, a vegetable and a mineral.')
+print(animal)
+print(vegetable)
+print(mineral)
